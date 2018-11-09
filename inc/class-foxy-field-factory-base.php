22b782fd6aee8e80e9dda333cc8b0a7d9f471910
Puleeno Nguyen <puleeno@gmail.com>
2018-11-09 08:17:58 +0700

    Updated generate fields content

--- inc/class-foxy-field-factory-base.php
+++ inc/class-foxy-field-factory-base.php
@@ -14,23 +14,93 @@ abstract class Foxy_Field_Factory_Base {
 
 	}
 
-	public function generate_tab_title() {
+	public function generate_tabs_wrap() {
+		$class_names = sprintf(
+			'foxy-tabs-wrap foxy-fields-tabs-wrap tabs-%s',
+			esc_attr( $this->tabs['style'] )
+		);
 
+		return array(
+			'context' => 'foxy-fields-tab-wrap',
+			'class'   => $class_names,
+		);
 	}
 
-	public function generate_class_names() {
-
+	public function generate_tab_title( $tab ) {
+		$title = '';
+		if ( ! empty( $tab['icon'] ) ) {
+			$title .= sprintf( '<span class="foxy-icon foxy-fields-icon %s"></span> ', esc_attr( $tab['icon'] ) );
+		} elseif ( ! empty( $tab['image'] ) ) {
+			$title .= sprintf(
+				'<span class="foxy-image-icon foxy-fields-image"><img src="%s" alt="%s"/></span> ',
+				esc_attr( $tab['image'] ),
+				esc_attr( $tab['title'] )
+			);
+		}
+		$title .= sprintf( '<span class="foxy-title-text foxy-fields-title">%s</span>', esc_html( $tab['title'] ) );
+		return $title;
 	}
 
-	public function generate_field_label() {
-	}
+
 
 	public function generate_tab_content() {
-
+		if ( empty( $this->tabs['fields'] ) ) {
+			return;
+		}
+		Foxy::ui()->tag( $this->generate_tabs_wrap() );
+		Foxy::ui()->tag(
+			array(
+				'name'    => 'ul',
+				'context' => 'foxy-field-tabs',
+				'class'   => 'foxy-tabs foxy-fields-tabs',
+			)
+		);
+		foreach ( $this->tabs['fields'] as $tab ) {
+			if ( empty( $tab['id'] ) ) {
+				continue;
+			}
+			Foxy::ui()->tag(
+				array(
+					'name'    => 'li',
+					'context' => 'foxy-fields-tab',
+					'class'   => 'foxy-tab foxy-fields-tab',
+				)
+			);
+			printf( '<a href="#%s">%s</a>', esc_attr( $tab['id'] ), $this->generate_tab_title( $tab ) ); // WPCS: XSS ok.
+			echo '</li>';
+		}
+		echo '</ul></div>';
 	}
 
 	public function generate_fields_content() {
+		if ( empty( $this->fields ) ) {
+			esc_html_e( "Don't has any fields for display", 'foxy' );
+			return;
+		}
+		foreach ($this->fields as $tab => $fields ) {
+			Foxy::ui()->tag( $this->generate_fields_wrap( $tab ) );
+			$this->generate_fields( $fields );
+			echo '</div>';
+		}
+	}
 
+	public function generate_fields( $fields ) {
+
+	}
+
+	abstract public function generate_field( $field );
+
+	public function generate_fields_wrap( $tab_id ) {
+
+	}
+
+	public function generate_tab_id() {
+	}
+
+	public function generate_field_class_names() {
+	}
+
+	public function generate_field_label() {
 	}
 
 	abstract public function manufacture();
--- inc/class-foxy-field-factory-post-meta.php
+++ inc/class-foxy-field-factory-post-meta.php
@@ -2,5 +2,7 @@
 
 class Foxy_Field_Factory_Post_Meta extends Foxy_Field_Factory_Base {
 	public function manufacture() {
+		$this->generate_tab_content();
+		$this->generate_fields_content();
 	}
 }
