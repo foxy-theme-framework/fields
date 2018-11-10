df3afd2216b57ea214e62c40b286235c7c30075f
Puleeno Nguyen <puleeno@gmail.com>
2018-11-10 22:48:01 +0700

    Finish structure for generate field content

--- inc/class-foxy-fields-base-field.php
+++ inc/class-foxy-fields-base-field.php
@@ -1,37 +1,90 @@
 <?php
 
 abstract class Foxy_Fields_Base_Field {
-	public function create_label( $field ) {
+	protected $data_parser;
+	protected $object;
+	protected $field;
+
+	public function __construct( $object, $field ) {
+		$this->object = $object;
+		$this->field = $field;
+
+		$this->data_parser = new Foxy_Fields_Data_Parser(
+			$this->object,
+			$this->field
+		);
+	}
+
+	public function generate_field_classes( $field ) {
+		$class_names = sprintf(
+			'field-type-%1$s field-%2$s',
+			esc_attr( $field['type'] ),
+			esc_attr( $field['id'] )
+		);
+
+		if ( ! empty( $field['title'] ) ) {
+			$class_names .= ' has-title';
+		}
+		if ( ! empty( $field['desc'] ) ) {
+			$class_names .= ' has-desc';
+		}
+		if ( ! empty( $field['subtitle'] ) ) {
+			$class_names .= ' has-subtitle';
+		}
+		// Free up memory.
+		unset( $field );
+
+		return $class_names;
+	}
+
+	public function field_label() {
+		$field  = $this->field;
+
 		$tag = wp_parse_args( $field['label_width'], array(
-			'echo' => false,
+			'context'         => 'foxy-field-label-wrap',
+			'class'           => 'foxy-label-wrap foxy-fields-label-wrap',
+			'mobile_columns'  => 12,
+			'tablet_columns'  => 4,
+			'desktop_columns' => 3,
 		));
-		$label = Foxy::ui()->tag($tag);
-			$label .= sprintf(
-				'<label for="foxy-field-%s" class="foxy-field-label">%s</label>',
-				$field['id'],
-				$field['title']
+		Foxy::ui()->tag( $tag );
+		Foxy::ui()->tag(
+			array(
+				'name'    => 'label',
+				'context' => 'foxy-fields-label',
+				'class'   => 'foxy-label foxy-fields-label',
+			),
+			array(
+				'for' => sprintf( 'foxy-fields-%s', esc_attr( $field['id'] ) ),
+			)
+		);
+		printf( '%s</label>', esc_html( $field['title'] ) );
+		if ( ! empty( $field['subtitle'] ) ) {
+			Foxy::ui()->tag(
+				array(
+					'context' => 'foxy-fields-subtitle',
+					'class'   => 'foxy-subtitle foxy-fields-subtitle',
+				)
 			);
-
-			if ( ! empty( $field['subtitle'] ) ) {
-				$label .= sprintf(
-					'<div class="foxy-field-subtitle">%s</div>',
-					$field['subtitle']
-				);
-			}
-		$label .= '</div>';
-
-		return $label;
+			printf( '%s</div>', esc_html( $field['subtitle'] ) );
+		}
+		echo '</div>';
 	}
 
-	public function create_desc( $field ) {
-		if ( ! empty( $field['desc'] ) ) :
-		?>
-		<div class="field-desc">
-			<?php echo esc_html( $field['desc'] ); ?>
-		</div>
-		<?php
-		endif;
+	public function field_desc() {
+		$field = $this->field;
+		if ( ! empty( $field['desc'] ) ) {
+			Foxy::ui()->tag( array(
+				'context' => 'foxy-fields-desc',
+				'class'   => 'foxy-desc foxy-fields-desc',
+			) );
+			printf( '%s</div>', esc_html() );
+		}
 	}
 
-	abstract public function output( $field );
+	public function output() {
+		$this->field_label();
+		$this->field_content();
+		$this->field_desc();
+	}
 }
--- inc/text/class-foxy-fields-text-field.php
+++ inc/text/class-foxy-fields-text-field.php
@@ -1,7 +1,32 @@
 <?php
 
 class Foxy_Fields_Text_Field extends Foxy_Fields_Base_Field {
-	public function output( $field ) {
-		echo 'blabla';
+	public function text( $id ) {
+		$value = $this->data_parser->value( $id );
+		?>
+		<input type="text" class="foxy-field" name="<?php echo esc_attr( $id ); ?>" value="<?php echo esc_attr( $value ); ?>">
+		<?php
+	}
+
+	public function field_content() {
+		$field = wp_parse_args(
+			$this->field,
+			array(
+				'content_width' => array(
+				)
+			)
+		);
+		Foxy::ui()->tag(
+			array(
+				'context' => 'foxy-fields-content',
+				'class'   => sprintf(
+					'foxy-content foxy-fields-content %s-type-content %s-content',
+					esc_attr( $field['type'] ),
+					esc_attr( $field['id'] )
+				),
+			)
+		);
+		$this->text( $field['id'] );
+		echo '</div>';
 	}
 }
