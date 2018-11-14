63e20b80f7ce29c6e1b6b363fff37a9b85b000c4
Puleeno Nguyen <puleeno@gmail.com>
2018-11-14 17:47:23 +0700

    Clean and udpate factory

---
+++ inc/class-foxy-fields-factory-option.php
@@ -0,0 +1,14 @@
+<?php
+
+class Foxy_Fields_Factory_Option {
+	$options = array();
+	$sections = array();
+	$fields = array();
+
+	public function add_fields( $id, $fields ) {
+
+	}
+
+	public function add_field( $id, $section, $fields ) {
+	}
+}
