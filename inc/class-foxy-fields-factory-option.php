d0b7d8d0abac97c5be9d989109c05ab60602a4fc
Puleeno Nguyen <puleeno@gmail.com>
2018-11-15 08:08:20 +0700

    Add foxy fields factory option

--- inc/class-foxy-fields-factory-option.php
+++ inc/class-foxy-fields-factory-option.php
@@ -1,9 +1,9 @@
 <?php
 
 class Foxy_Fields_Factory_Option {
-	$options = array();
-	$sections = array();
-	$fields = array();
+	protected $options = array();
+	protected $sections = array();
+	protected $fields = array();
 
 	public function add_fields( $id, $fields ) {
 
